0c27180c31ecb97d51897b37a1cbcd757c3379ef
Johannes Diemke <johannes.diemke@eventim.de>
2018-06-17 21:10:38 +0200

    Add basic spring boot application scaffold

---
+++ backend/src/main/java/io/github/jdiemke/ngforum/Application.java
@@ -0,0 +1,18 @@
+package io.github.jdiemke.ngforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the entry point for our Spring Boot 2 application.
+ * 
+ * @author Johannes Diemke
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
